26e806234ddc8162b778bf149b4356300e4eb199
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-03 15:08:29 +0000

    Update reverseString.js

--- JS/Strings/reverseString.js
+++ JS/Strings/reverseString.js
@@ -1,1 +1,24 @@
 
+
+
+/**
+ * Reverses a given string.
+ *
+ * @param {string} string - The input string to be reversed.
+ * @returns {string} - The reversed string.
+ * @throws {Error} - If the input is not a string or if it is empty.
+ */
+
+
+
+const reverseString = (string) => {
+    if (typeof string !== "string") {
+        throw new Error("Input must be a string");
+    }
+    if(string.length === 0) {
+        throw new Error("Input must not be empty");
+    }
+  return string.split("").reverse().join("");
+};
+
+module.exports = reverseString;
